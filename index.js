3598ed877aeb872d757d87738531e91c3d575411
CelJudy <celjudygmail.com>
2025-03-20 01:15:37 -0600

    index optimization

--- index.js
+++ index.js
@@ -14,15 +14,11 @@ app.set("view engine", "ejs");
 let alumnos=require("./alumnos.json");
 
 io.on('connection', (socket)=>{
-    //console.log("usuario conectado", socket.handshake.query.user);
-    //console.log("ips", ips);
-    //console.log("ip", socket.handshake.address);
 
     if(socket.handshake.query.user=="celeste"){
         io.emit('list', alumnos);
     }else{
         if(alumnos.find((item)=>item.ip==socket.handshake.address)===undefined){
-
             alumnos[socket.handshake.query.user].online=true;
             alumnos[socket.handshake.query.user].ip=socket.handshake.address;
             io.emit('check', 1);
@@ -30,39 +26,13 @@ io.on('connection', (socket)=>{
         }
     }
 
-    /* if(ips.indexOf(socket.handshake.address)<0){
-        ips.push(socket.handshake.address);
-        //console.log("ips conectadas", ips);
-        if(socket.handshake.query.user!="celeste"){
-            io.emit('online', socket.handshake.query.user);
-        }
-    } */
-
-        /* if(socket.handshake.query.user!="celeste"){
-            io.emit('online', socket.handshake.query.user);
-        } */
-
     socket.on('disconnect', ()=>{
         if(socket.handshake.query.user!="celeste"){
             alumnos[socket.handshake.query.user].online=false;
             alumnos[socket.handshake.query.user].ip="";
             io.emit('offline', socket.handshake.query.user);
         }
-        //console.log(alumnos);
-        /* const index=ips.indexOf(socket.handshake.address);
-        //console.log("antes de eliminar", ips);
-        //console.log("index", socket.handshake.address);
-        if(index>=0){
-            ips.splice(index, 1);
-            //console.log("despues de eliminar", ips);
-        } */
     });
-
-    
-
-    /* socket.on('online', (value)=>{
-        console.log("online", value);
-    }); */
     
 });
 
